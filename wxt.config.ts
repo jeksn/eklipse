b3dfb22f00143452f5787aaacee216335c98f1bf
Johan <johan@jeksn.me>
2026-04-22 14:55:02 +0200

    feat: add icons

--- wxt.config.ts
+++ wxt.config.ts
@@ -13,10 +13,10 @@ export default defineConfig({
       default_title: 'Eklipse',
     },
     icons: {
-      16: 'icon/16.png',
-      32: 'icon/32.png',
-      48: 'icon/48.png',
-      128: 'icon/128.png',
+      16: 'eklipse-icon/144.png',
+      32: 'eklipse-icon/144.png',
+      48: 'eklipse-icon/144.png',
+      128: 'eklipse-icon/144.png',
     },
   },
 });
